3782f65a20dfdd13c374da92ef503a63d33e860b
Prajakta Kulkarni <prajaktakulkarni@Prajaktas-Mini.lan>
2020-02-09 00:29:34 +0100

    changes to show twitter feed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:twitter_api/twitter_api.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,105 +12,76 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => new HomePageState();
+  
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class HomePageState extends State<HomePage>{
+  Future<String> getFeeds() async{
+    String consumerApiKey = "UoA2TXUyi0NngaW5a7aUqsRgx";
+    String consumerApiSecret = "FN5mxRQcJ0nsXEnPxzTqt03zqRq5UIJqaxQOlciDoTbA8D0Ehi";
+    String accessToken = "96678665-L6qvwfLtREKlzsUquNtRivPqfAkbeUjudBBbANAT5";
+    String accessTokenSecret = "tDFUgkIRXeyV7hfwFB3WzOWbznITiNPlCf0ilEbBqfJe7";
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    final _twitterOauth = new twitterApi(
+      consumerKey: consumerApiKey,
+     consumerSecret: consumerApiSecret,
+     token: accessToken,
+      tokenSecret: accessTokenSecret
+    );
+
+    Future twitterRequest = _twitterOauth.getTwitterRequest(
+      // Http Method
+      "GET", 
+      // Endpoint you are trying to reach
+      "statuses/user_timeline.json", 
+      // The options for the request
+      options: {
+        "user_id": "19025957",
+        "screen_name": "TTCnotices",
+        "count": "20",
+        "trim_user": "true",
+        "tweet_mode": "extended", // Used to prevent truncating tweets
+      },
+    );
+
+    var res = await twitterRequest;
+
+    print(res.statusCode); 
+    print(res.body);
+
+    var tweets = json.decode(res.body);
+    print(tweets);
+
+
+    // var response = await http.get(
+    //   Uri.encodeFull("https://jsonplaceholder.typicode.com/posts"),
+    //   //Uri.encodeFull("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=kPrajakta&count=2"),
+    //   headers: {
+    //     "Accept": "application/json"
+    //   }
+    // );
+
+    // print(response.body);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return new Scaffold(
+      body: new Center(
+        child: new RaisedButton(
+          child: new Text("Get Feeds"),
+          onPressed: getFeeds,
+        )
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
 }
